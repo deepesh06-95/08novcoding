2c87c9cd2ccd6dbf0e1efe839e96d820ba588c21
deepesh06-95 <91532309+deepesh06-95@users.noreply.github.com>
2021-11-08 15:34:42 +0530

    food app

---
+++ projectmyown/foodapp/components/navbar.js
@@ -0,0 +1,22 @@
+
+function navbar()
+{
+    return `
+    
+    <h3><a href="/">Home</a></h3>
+
+    <div id="ser">
+    <input id="Search" type="text" placeholder="Search"/>
+    <button id="btn">Search</button>
+    </div>
+
+     <div id="options">
+     <h3><a href="menu.html">Menu</a></h3>
+     <h3><a href="login.html">Login</a></h3>
+     <h3><a href="signup.html">Signup</a></h3>
+     </div>
+
+    `
+}
+
+export default navbar
